08aaa69c48dff3506c29910747e409df9eab727e
fabio loterio <vitorloterio@gmail.com>
2020-12-09 21:57:55 -0300

    migration ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    MainComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
